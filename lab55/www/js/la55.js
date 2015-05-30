04162169182725aaa5531251f87e6ae949a71560
lucia13 <lucerolucia@gmail.com>
2015-05-29 21:51:37 -0500

    device y camera

---
+++ lab55/www/js/la55.js
@@ -0,0 +1,15 @@
+var info = document.getElementById('deviceProperties');
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+    // Cordova is ready
+    //
+    function onDeviceReady() {
+        var element = document.getElementById('deviceProperties');
+
+        element.innerHTML = 'Device Name: '     + device.name     + '<br />' + 
+                            'Device Cordova: '  + device.cordova  + '<br />' + 
+                            'Device Platform: ' + device.platform + '<br />' + 
+                            'Device UUID: '     + device.uuid     + '<br />' + 
+                            'Device Version: '  + device.version  + '<br />';
+    }
